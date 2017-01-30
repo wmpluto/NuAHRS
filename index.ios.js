148462246d25f90ce1b5759eeb2d3fd6624be8a5
eve <wmpluto@gmail.com>
2017-01-31 01:39:33 +0800

    show ble devices lists

--- index.ios.js
+++ index.ios.js
@@ -9,21 +9,31 @@
    AppRegistry,
    Text,
    View,
+   ListView,
+   StyleSheet,
    TouchableHighlight,
    NativeAppEventEmitter,
    Platform,
    PermissionsAndroid
  } from 'react-native';
- import BleManager from 'react-native-ble-manager';
+import BleManager from 'react-native-ble-manager';
+import ScannedDeviceView from './app/components/ScannedDeviceView';
 
  class NuAHRS extends Component {
 
      constructor(){
          super()
 
+         var ds = new ListView.DataSource({
+             rowHasChanged: (r1, r2) => r1 !== r2,
+             sectionHeaderHasChanged: (s1, s2) => s1 !== s2
+         });
+
          this.state = {
              ble:null,
              scanning:false,
+             dataSource:ds,
+             devices: new Array
          }
      }
 
@@ -67,8 +77,35 @@
      }
 
      handleDiscoverPeripheral(data){
-         console.log('Got ble data', data);
          this.setState({ ble: data })
+         for (device of this.state.devices) {
+            if (data.id == device.id) {
+                return
+            }
+         }
+         this.state.devices.push(data)
+     }
+
+     _renderRow(rowData) {
+         if (rowData.hasOwnProperty("name")) {
+             return (
+              <ScannedDeviceView
+                name={rowData.name}
+                uuid={rowData.id}
+                rssi={rowData.rssi}
+                onClick={null}
+              />
+            )
+        } else {
+            return (
+             <ScannedDeviceView
+               name={null}
+               uuid={rowData.id}
+               rssi={rowData.rssi}
+               onClick={null}
+             />
+           )
+        }
      }
 
      render() {
@@ -78,19 +115,19 @@
              justifyContent: 'center',
              alignItems: 'center',
              backgroundColor: '#F5FCFF',
+             padding: 20,
          }
-
-         const bleList = this.state.ble
-             ? <Text> Device found: {this.state.ble.name} </Text>
-             : <Text>no devices nearby</Text>
 
          return (
              <View style={container}>
+                 <ListView style={{flex:1, margin:20}}
+                     enableEmptySections={true}
+                     dataSource={this.state.dataSource.cloneWithRows(this.state.devices)}
+                     renderRow={(rowData, sectionId, rowId) => this._renderRow(rowData)}
+                     />
                  <TouchableHighlight style={{padding:20, backgroundColor:'#ccc'}} onPress={() => this.toggleScanning(!this.state.scanning) }>
                      <Text>Scan Bluetooth ({this.state.scanning ? 'on' : 'off'})</Text>
                  </TouchableHighlight>
-
-                 {bleList}
              </View>
          );
      }
